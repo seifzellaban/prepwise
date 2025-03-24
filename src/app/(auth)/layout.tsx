2d4dbe9607d4a798d3721b861b7e61da39b9c88b
Mason <seifzellaban@gmail.com>
2025-03-24 20:39:42 +0200

    Setup app, files, folders and some of the routing

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="auth-layout">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,9 @@
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+export default RootLayout;
