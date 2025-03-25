b302bb2d9ced7249a4db654d51b3ad17a36e5af2
Mason <seifzellaban@gmail.com>
2025-03-25 21:06:24 +0200

    Implemented auth and storage

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,12 @@
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="auth-layout">
-      {children}
-    </div>
-  );
+import { isAuthenticated } from '@/lib/actions/auth.actions';
+import { redirect } from 'next/navigation';
+
+const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (isUserAuthenticated) redirect('/');
+
+  return <div className='auth-layout'>{children}</div>;
 };
 
 export default AuthLayout;
--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,56 +1,99 @@
 'use client';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+
 import { z } from 'zod';
-import { Button } from '@/components/ui/button';
-import { Form } from '@/components/ui/form';
-import Image from 'next/image';
 import Link from 'next/link';
+import Image from 'next/image';
 import { toast } from 'sonner';
-import FormField from './FormField';
+import { auth } from '@/firebase/client';
+import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+
+import { Form } from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+
+import FormField from './FormField';
+import { signIn, signUp } from '@/lib/actions/auth.actions';
 
 const authFormSchema = (type: FormType) => {
   return z.object({
-    username:
-      type === 'sign-up'
-        ? z.string().min(2).max(32)
-        : z.string().min(2).max(32).optional(),
+    name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
     email: z.string().email(),
-    password: z.string().min(8).max(32),
+    password: z.string().min(3),
   });
 };
 
 const AuthForm = ({ type }: { type: FormType }) => {
   const router = useRouter();
+
   const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: '',
+      name: '',
       email: '',
       password: '',
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     try {
       if (type === 'sign-up') {
-        toast.success(
-          'Account created successfully, please sign in.'
+        const { name, email, password } = data;
+
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
         );
+
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
+        }
+
+        toast.success('Account created successfully. Please sign in.');
         router.push('/sign-in');
-        console.log('SIGN UP', values);
       } else {
+        const { email, password } = data;
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+        if (!idToken) {
+          toast.error('Sign in Failed. Please try again.');
+          return;
+        }
+
+        await signIn({
+          email,
+          idToken,
+        });
+
         toast.success('Signed in successfully.');
         router.push('/');
-        console.log('SIGN IN', values);
       }
     } catch (error) {
-      console.error(error);
+      console.log(error);
       toast.error(`There was an error: ${error}`);
     }
-  }
+  };
 
   const isSignIn = type === 'sign-in';
 
@@ -58,15 +101,12 @@ const AuthForm = ({ type }: { type: FormType }) => {
     <div className='card-border lg:min-w-[566px]'>
       <div className='flex flex-col gap-6 card py-14 px-10'>
         <div className='flex flex-row gap-2 justify-center'>
-          <Image
-            src='/logo.svg'
-            alt='logo'
-            height={32}
-            width={38}
-          />
+          <Image src='/logo.svg' alt='logo' height={32} width={38} />
           <h2 className='text-primary-100'>PrepWise</h2>
         </div>
-        <h3>Practice job inteviews with AI</h3>
+
+        <h3>Practice job interviews with AI</h3>
+
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
@@ -74,40 +114,43 @@ const AuthForm = ({ type }: { type: FormType }) => {
           >
             {!isSignIn && (
               <FormField
-                name='username'
                 control={form.control}
+                name='name'
                 label='Name'
                 placeholder='Your Name'
+                type='text'
               />
             )}
+
             <FormField
-              name='email'
               control={form.control}
+              name='email'
               label='Email'
-              placeholder='Your Email'
+              placeholder='Your email address'
               type='email'
             />
+
             <FormField
-              name='password'
               control={form.control}
+              name='password'
               label='Password'
-              placeholder='Your Password'
+              placeholder='Enter your password'
               type='password'
             />
+
             <Button className='btn' type='submit'>
-              {!isSignIn ? 'Sign In' : 'Create Account'}
+              {isSignIn ? 'Sign In' : 'Create an Account'}
             </Button>
           </form>
         </Form>
+
         <p className='text-center'>
-          {isSignIn
-            ? 'No account yet?'
-            : 'Have an account already'}
+          {isSignIn ? 'No account yet?' : 'Have an account already?'}
           <Link
             href={!isSignIn ? '/sign-in' : '/sign-up'}
             className='font-bold text-user-primary ml-1'
           >
-            {!isSignIn ? 'Sign in' : 'Sign up'}
+            {!isSignIn ? 'Sign In' : 'Sign Up'}
           </Link>
         </p>
       </div>
